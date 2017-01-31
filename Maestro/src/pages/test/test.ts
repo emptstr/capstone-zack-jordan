49a72f7b16b923f4c0bc983e06839b0f3105aff2
Zachary Walton <ztwalto@gmail.com>
2017-01-31 03:34:30 -0500

    Angular2 support for webstorm

--- Maestro/src/pages/test/test.ts
+++ Maestro/src/pages/test/test.ts
@@ -2,11 +2,11 @@ import { Component } from '@angular/core';
 
 
 @Component({
-    selector: 'test-page',
-    templateUrl: 'test.html'
+  selector: 'test-page',
+  templateUrl: 'test.html'
 })
 export class test {
-    constructor(){
+  constructor(){
 
-    }
+  }
 }
