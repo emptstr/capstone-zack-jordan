ac11a33d6cad179193141c2c3fc162c5027bc1ca
Zachary Walton <ztwalto@gmail.com>
2017-02-06 17:20:38 -0500

    Implemented Login Page

--- Maestro/src/pages/login/login.ts
+++ Maestro/src/pages/login/login.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import { NavController, AlertController, LoadingController, Loading } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
+import { RegisterPage } from '../register/register';
+import { HomePage } from '../home/home';
 
 @Component({
-  selector: 'login-page',
+  selector: 'page-login',
   templateUrl: 'login.html'
 })
 export class LoginPage {
+  loading: Loading;
+  registerCredentials = {email: '', password: ''};
 
-  constructor(){
+  constructor(
+    private nav: NavController,
+    private auth: AuthService,
+    private alertCtrl: AlertController,
+    private loadingCtrl: LoadingController
+  ){}
 
+  public createAccount(){
+    this.nav.push(RegisterPage);
+  }
+
+  public login() {
+    this.showLoading()
+    this.auth.login(this.registerCredentials).subscribe(allowed => {
+      if (allowed) {
+        setTimeout(() => {
+          this.loading.dismiss();
+          this.nav.setRoot(HomePage);
+        });
+      } else {
+        this.showError("Access Denied");
+      }
+    },
+    error => {
+      this.showError(error);
+    });
+  }
+
+  showLoading() {
+    this.loading = this.loadingCtrl.create({
+      content: 'Please wait...'
+    });
+    this.loading.present();
+  }
+
+  showError(text) {
+    setTimeout(() => {
+      this.loading.dismiss();
+    });
+
+    let alert = this.alertCtrl.create({
+      title: 'Fail',
+      subTitle: text,
+      buttons: ['OK']
+    });
+    alert.present(prompt);
   }
 }
