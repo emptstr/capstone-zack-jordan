30a2fd9aa12a4b8726345ef8c44ecc611478a4bc
Zachary Walton <ztwalto@gmail.com>
2017-02-27 16:49:58 -0500

    Updated session display method to be more dynamic

--- Maestro/src/pages/home/home.ts
+++ Maestro/src/pages/home/home.ts
@@ -1,6 +1,6 @@
 import {Component} from '@angular/core';
-import {NavController} from 'ionic-angular';
-import { AuthService } from '../../providers/auth-service';
+import {NavController, AlertController} from 'ionic-angular';
+//import { AuthService } from '../../providers/auth-service';
 import { NewSessionPage } from '../new-session/new-session';
 
 @Component({
@@ -10,13 +10,45 @@ import { NewSessionPage } from '../new-session/new-session';
 export class HomePage {
   session_name = '';
   email = '';
-  constructor(private nav: NavController, private auth: AuthService) {}
+  sessions = [];
+  constructor(private nav: NavController, private alertCtrl: AlertController) {
+    //Hard coded data for now
+    this.sessions = [
+      {session_title: 'Work Session 1',
+       session_time: '01:20:20',
+       session_description: 'Today I worked on...' },
+      {session_title: 'Work Session 2',
+        session_time: '01:20:20',
+        session_description: 'Today I worked on...' }];
+  }
 
   /**
    * Navigates to New Session Page
    */
-  public newSession(){
+  newSession(){
     this.nav.push(NewSessionPage);
+  }
+
+  deletePrompt(){
+    let prompt = this.alertCtrl.create({
+      title: 'Delete Session',
+      message: "Are you sure you would like to delete this work session?",
+      buttons: [
+        {
+          text: 'Yes',
+          handler: data => {
+            console.log('Delete Session');
+          }
+        },
+        {
+          text: 'No',
+          handler: data => {
+            console.log('Cancel Delete Session');
+          }
+        }
+      ]
+    });
+    prompt.present();
   }
 
 }
