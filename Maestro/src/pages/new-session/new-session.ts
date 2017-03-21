17069527349943f757736aa70c866c9590114efe
Zachary Walton <ztwalto@gmail.com>
2017-03-20 23:58:06 -0400

    Implemented timer and put Session info into session object

--- Maestro/src/pages/new-session/new-session.ts
+++ Maestro/src/pages/new-session/new-session.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { Observable, Subscription } from 'rxjs/Rx';
 
 
 @Component({
@@ -7,40 +8,55 @@ import { NavController } from 'ionic-angular';
   templateUrl: 'new-session.html'
 })
 export class NewSessionPage {
-  session_title: string = '';
-  session_notes: string = '';
-  start_time: string;
-  end_time: string;
+  session: any = {};
   started: boolean;
   end_session: boolean;
+  subscription: Subscription;
 
   constructor(public nav: NavController) {
+    this.session = {
+      title: '',
+      notes: '',
+      time: '',
+      start_time: '',
+      end_time: '',
+    };
+
     this.started = false;
     this.end_session = false;
   }
 
   startSession(){
+    let timer = Observable.timer(0, 1000);
+    this.subscription = timer.subscribe(t => this.session.time = convertSec(t));
     this.started = true;
     console.log("Session Started");
-    this.start_time = getDateTime();
+    this.session.start_time = getDateTime();
+
   }
 
   endSession(){
+    this.subscription.unsubscribe();
     this.started = false;
     this.end_session = true;
     console.log("Session Ended");
-    this.end_time = getDateTime();
+    this.session.end_time = getDateTime();
   }
 
   saveSession(){
-    console.log("Session Title: " + this.session_title);
-    console.log("Session Notes: " + this.session_notes);
-    console.log("Start Time: " + this.start_time);
-    console.log("End Time: " + this.end_time);
+    this.subscription.unsubscribe();
+    console.log("Session Title: " + this.session.title);
+    console.log("Session Notes: " + this.session.notes);
+    console.log("Start Time: " + this.session.start_time);
+    console.log("End Time: " + this.session.end_time);
+    console.log("Time of session: " + this.session.time);
   }
 
-
 }
+
+
+const convertSec = ticks => { return new Date(ticks * 1000).toISOString().substr(11, 8); };
+
 
 const getDateTime = () => {
   let dateTime;
@@ -48,7 +64,8 @@ const getDateTime = () => {
   dateTime = date.toLocaleDateString();
   dateTime += " " + date.toLocaleTimeString();
   return dateTime;
-}
+};
+
 
 
 
