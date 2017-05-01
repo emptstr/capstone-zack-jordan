597126db78f2c4272d9b39a6f24a2a111681afe9
Zachary Walton <ztwalto@gmail.com>
2017-05-01 11:42:26 -0400

    commented session-info

--- Maestro/src/pages/session-info/session-info.ts
+++ Maestro/src/pages/session-info/session-info.ts
@@ -3,28 +3,42 @@ import {NavController, NavParams} from 'ionic-angular';
 import { SessionService } from '../../providers/sessions/session.service';
 
 
-
+/**
+ * Component for displaying more info about a session.
+ *
+ * Accesses from list-session when more button is pressed.
+ */
 @Component({
   selector: 'page-session-info',
   templateUrl: 'session-info.html'
 })
+
 export class SessionInfoPage {
 
-  public session;
-  public edit: boolean = false;
-  constructor(public navCtrl: NavController, public navParams: NavParams, public sess: SessionService) {
-    this.session = navParams.get("session");
-    console.log(this.session);
+  public session; // Session being accessed
+  public edit: boolean = false; // True if edit button is pressed
+
+  constructor(public navParams: NavParams, public sess: SessionService) {
+    this.session = navParams.get("session"); // Get session passed from list-session component
+    console.log(this.session); // Testing
   }
 
+  /**
+   * Click handler for edit session button.
+   *
+   * Displays session information in editable form fields
+   */
   editSession(){
     this.edit = true;
   }
 
+  /**
+   * Click handler for save button after session is edited
+   */
   saveSession(){
-    this.sess.addSession(this.session);
-    console.log(this.session);
-    this.edit = false;
+    this.sess.addSession(this.session); // Update session information
+    console.log(this.session);  // Testing
+    this.edit = false;  // Editing is done
   }
 
 }
